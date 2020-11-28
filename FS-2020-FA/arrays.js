19574c2a48046d13d8d8c47e2b5981e074b00e88
kalmansour <khaledalmansour24@gmail.com>
2020-11-28 12:16:54 +0300

    first commit

---
+++ FS-2020-FA/arrays.js
@@ -0,0 +1,82 @@
+// const instructorOne = "Laila";
+// const instructorOne = "Mshary";
+// const instructorOne = "Ahmed";
+// const instructorOne = "Aziz";
+
+// console.log("The number of instructors is 4")
+
+// let instructors = ["Laila", "Mshary", "Ahmed"];
+
+// console.log(instructors);
+// console.log(instructors[0]);
+// console.log(instructors[1]);
+// console.log(instructors[2]);
+
+// const numberOfInstructors = instructors.length;
+// console.log(numberOfInstructors);
+//OR
+// console.log(instructors.length)
+
+// SYNTAX
+// Array.push(newElement)
+
+// let instructors = ["Laila", "Mshary", "Ahmed"];
+
+// instructors.push("Aziz");
+// console.log(instructors);
+
+// SYNTAX
+// const lastElement = array.pop()
+
+// const squeezedOranges = [];
+
+// function makeOrangeJuice(){
+//     squeezedOranges.push(instructors.pop());
+// }
+
+// squeezedOranges.push(instructors.pop());
+// squeezedOranges.push(instructors.pop());
+
+// console.log("INSTRUCTORS", instructors);
+// console.log("SQUEEZEDORANGES", squeezedOranges);
+
+// const lastOne = instructors.pop();
+// console.log(lastOne);
+
+// console.log(instructors.pop());
+
+// console.log(instructors);
+
+// console.log(instructors);
+
+// let instructors = ["Laila", "Mshary", "Hasan", "Aziz"];
+
+// SYNTAX
+// array.slice()
+
+// console.log(instructors.slice(2));
+
+// console.log(instructors.slice(0, -2));
+// console.log(instructors.slice(1, -1));
+
+// console.log(instructors);
+
+// SYNTAX
+// while(){
+// CODE;
+// }
+
+// let counter = 0;
+
+// while (counter < 6) {
+//   console.log(counter);
+//   counter = counter + 1;
+// }
+
+// const instructors = ["Laila", "Mshary", "Hasan", "Aziz", "Hamza"];
+
+// let i = 0;
+// while (i < instructors.length) {
+//   console.log(`Hello, I'm ${instructors[i]}`);
+//   i++;
+// }
---
+++ FS-2020-FA/variables.js
@@ -0,0 +1,31 @@
+// console.log("Hello World"); //In JS we can print strings
+
+// console.log(3); //and we can print numbers
+// console.log(`3`);
+
+// console.log(3 + 5); //treat them as numbers ----> 8
+// console.log(`3 + 5`); // treat them as strings ----> 3 + 5
+
+// let numberOfRepos = 500;
+// let username = "TheMshary";
+
+// console.log(`The facebook username is ` + username);
+// console.log(`The number of reps is` + numberOfRepos);
+
+// console.log(`The facebook username is ${username}`);
+// console.log(`The number of reps is ${numberOfRepos}`);
+
+// console.log(`The Github username is ` + username); //concatination
+// console.log(`${username} has 30 repos ` + username); //concatination
+
+// console.log(`The Github username is ${username}`); //interpelating
+// console.log(`${username} has ${numberOfRepos} repos`); //interpelating
+
+username = `Lailz`;
+let numberOfRepos = 500;
+console.log(`The Github username is ${username}`); //interpelating
+console.log(`${username} has ${numberOfRepos + 5} repos`); //interpelating
+
+//interpelating method is much more efficient. Use that.
+
+// const username = "TheMshary" //if i don't want the variable to change use const instead "Sealing the box"
