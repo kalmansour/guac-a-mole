9e83790330899855b763e059503769a0f5fc3216
kalmansour <khaledalmansour24@gmail.com>
2021-04-06 08:01:35 +0300

    home styles

--- guac-a-mole/src/components/home/Home.js
+++ guac-a-mole/src/components/home/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 
+// Styles
+import { Container, Title, Subtitle, Button } from "./styles";
 const Home = () => {
   return (
     <container>
@@ -19,36 +20,3 @@ const Home = () => {
 };
 
 export default Home;
-
-const Container = styled.div`
-  text-align: center;
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-`;
-
-const Title = styled.h1`
-  font-weight: bold;
-  font-size: 62px;
-  text-align: "center";
-  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-`;
-
-const Subtitle = styled.p`
-  font-size: 40px;
-  text-align: "center";
-  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
-`;
-
-const Button = styled.button`
-  font-size: 50px;
-  background-color: white;
-  border: solid;
-  border-width: 4px;
-  border-color: #32cd32;
-  color: #32cd32;
-  margin: 20px 20px;
-  height: 200px;
-  width: 400px;
-`;
---
+++ guac-a-mole/src/components/home/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  text-align: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+`;
+
+export const Title = styled.h1`
+  font-weight: bold;
+  font-size: 62px;
+  text-align: "center";
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+`;
+
+export const Subtitle = styled.p`
+  font-size: 40px;
+  text-align: "center";
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+`;
+
+export const Button = styled.button`
+  font-size: 50px;
+  background-color: white;
+  border: solid;
+  border-width: 4px;
+  border-color: #32cd32;
+  color: #32cd32;
+  margin: 20px 20px;
+  height: 200px;
+  width: 400px;
+`;
